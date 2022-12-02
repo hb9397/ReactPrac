e0112e8109e0bc9f2d6c58cc8bbe6c48aa4db24b
hb9397@naver.com <hb9397@naver.com>
2022-12-02 17:39:40 +0900

    React (React 개요, 컴포넌트 생성 방식) 20221202-2

--- react_prac1/src/App.js
+++ react_prac1/src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    const message = "클래스형 컴포넌트 - 과거에 많이 사용함.";
+    return <div className='react'>{message}</div>
+  }
 }
 
 export default App;
