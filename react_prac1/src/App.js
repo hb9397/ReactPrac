7d8e313234e6efcfb203a52c8edac228df9242b4
hb9397@naver.com <hb9397@naver.com>
2022-12-05 17:56:11 +0900

    React (클래스형_Ref) 20221205-8

--- react_prac1/src/App.js
+++ react_prac1/src/App.js
@@ -1,27 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 import TEST from './test'
 import STATE from './component/StateClassType'
 import USESTATE from './component/StateFuncType'
 //import EventPracClass from './component/EventPracClass'
 import EventPracFunc from './component/EventPracFunc'
 import ValidationSample from './component/ValidationSample'
+import ScrollBox from './component/ScrollBox'
 
-
-const App = () => {
-  const message = " - 클래스형 컴포넌트는 과거에 많이 사용함, 현재는 함수형 컴포넌트를 많이 사용";
-  return (
-    <>
-      <div> 컴포넌트 {message}</div><br/>
-      <TEST name="Adam" year={199}>태그 안의 내용 사용하기</TEST>
-      <STATE/><br/>
-      <USESTATE /><br/>
-      {/* 클래스형 컴포넌트 이벤트 처리 */}
-      {/* <EventPracClass/> */}
-      <EventPracFunc/><br/>
-      <ValidationSample/>
-
-    </>
-  )
+const message = " - 클래스형 컴포넌트는 과거에 많이 사용함, 현재는 함수형 컴포넌트를 많이 사용";
+class App extends Component {
+  render() {
+    return (
+      <>
+        <div> 컴포넌트 {message}</div><br />
+        <TEST name="Adam" year={199}>태그 안의 내용 사용하기</TEST>
+        <STATE /><br />
+        <USESTATE /><br />
+        {/* 클래스형 컴포넌트 이벤트 처리 */}
+        {/* <EventPracClass/> */}
+        <EventPracFunc /><br />
+        <ValidationSample /><br />
+        <ScrollBox ref={ref => { this.box = ref }} />
+        <button onClick={this.box.scrollToBottom}>맨 아래로</button>
+      </>
+    )
+  }
 }
 
 export default App
