5f6dce06d3530cee233764613da1b10f73a3a3fa
hb9397@naver.com <hb9397@naver.com>
2022-12-05 15:44:19 +0900

    React (클래스형_이벤트 헨들링) 20221205-5

--- react_prac1/src/component/EventPrac.jsx
+++ react_prac1/src/component/EventPrac.jsx
@@ -1,39 +1,52 @@
 import React, { Component } from 'react'
 
 class EventPrac extends Component {
-    /* state = {
+    state = {
         name : '',
+        message: '',
         btnAble : true
-    } */
-
-    constructor(props){
-        super(props);
-        this.changeInput = this.changeInput.bind(this);
-        this.btnAble = this.btnAble.bind(this);
     }
 
     render() {
         const changeInput = (e) => {
-            this.setState({name: e.target.value})
+            this.setState({
+                // 이벤트가 발생한 객체는 e.target
+                // e.target.name은 이벤트가 발생한 객체의 name
+                [e.target.name]: e.target.value
+            })
             this.setState({btnAble: false})
         }
 
         const btnOnClick = (e) => {
-            alert(this.state.name);
+            alert(this.state.name + ":" + this.state.message);
             this.setState({
                 name: "",
-                btnAble: true
+                message: ''
             })
+            this.setState({btnAble: true})
+        }
+
+        const inputKeyPress = (e) =>{
+            if(e.key === 'Enter'){
+                btnOnClick();
+            }
         }
 
         return (
             <div>
                 <h1>이벤트 연습</h1>
-                <input type='text' name='message' placeholder='이름을 입력하세요' value={this.state.name}
-                    onChange={this.changeInput}>
+                <input type='text' name='name' placeholder='이름을 입력하세요' value={this.state.name}
+                    onChange={changeInput} onKeyPress={inputKeyPress}>
                 </input>
-                <button disabled = {this.state.able} onClick={this.btnOnClick}>확인</button><br/>
-                <textarea value={this.state.name} cols="30" rows="10"></textarea>
+                <textarea value={this.state.name} cols="13" rows="1"></textarea><br/>
+
+                <input type='text' name='message' placeholder='메시지를 입력하세요' value={this.state.message}
+                    onChange={changeInput} onKeyPress={inputKeyPress}>
+                </input>
+                <textarea value={this.state.message} cols="13" rows="1"></textarea><br/>
+
+                <button disabled = {this.state.btnAble} onClick={btnOnClick}>확인</button><br/>
+                
             </div>
         )
     }
