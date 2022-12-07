c3a7a6d0b48d739a8260a232cab3d29f95c88a25
hb9397@naver.com <hb9397@naver.com>
2022-12-07 13:55:54 +0900

    ReactStyling (CSS Module, SASS_SCSS) 20221207-5

--- react_prac3/src/App.js
+++ react_prac3/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import  CSSModule  from './component/CSSModule'
 
+import classNames from 'classnames'
+import styles from './App.scss'
+
+const cx = classNames.bind(styles)
+
 const App = () => {
+  const isBlue = true;
+
+
   return (
     <div>
-      <CSSModule/>
+      <CSSModule/><br/>
+      <div className={cx('box', {blue: isBlue})}>
+        <div className={cx('box-inside')}/>
+      </div>
+      <div className={cx('box-inside')}/> {/* App.scss에서 box의 자식으로 만들었기 때문에 부모에 box클래스를 가져오지 않는다면 설정되지 않는다. */}
     </div>
   )
 }
