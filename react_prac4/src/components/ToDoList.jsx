6159255448087ba9c901bd102db3479e5e7be34b
hb9397@naver.com <hb9397@naver.com>
2022-12-08 17:06:17 +0900

    React (ToDo Clone Coading - 최적화_대량 샘플 데이터 다루기_지연로딩, 데이터의 불변성)20221208-5

--- react_prac4/src/components/ToDoList.jsx
+++ react_prac4/src/components/ToDoList.jsx
@@ -1,20 +1,36 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import ToDoListItem from './ToDoListItem'
-
 import './ToDoList.scss'
+import { List } from 'react-virtualized'
 
-const ToDoList = ({toDos, onClickRemoveBtn, onClickCheckBoxToggle}) => {
-    
-  return (
-    <div className='ToDoList'>
-        {
-            // App.js로 부터 넘어온 toDos를 map함수로 toDos의 요소 개수 만큼 출력하도록 수정
-            toDos.map(toDo => (
-                <ToDoListItem toDo={toDo} key={toDo.id} onClickRemoveBtn={onClickRemoveBtn} onClickCheckBoxToggle={onClickCheckBoxToggle}/>
-            ))
-        }
-    </div>
-  )
+const ToDoList = ({ toDos, onClickRemoveBtn, onClickCheckBoxToggle }) => {
+
+    // 하나의 항목을 랜더링하기 위한 함수를 생성한다.
+    const rowRenderer = useCallback(({ index, key, style }) => {
+        // 출력할 데이터를 가져온다
+        const toDo = toDos[index];
+        console.log(toDo.length)
+        return (
+            <ToDoListItem
+                toDo={toDo}
+                key={key}
+                onClickRemoveBtn={onClickRemoveBtn}
+                onClickCheckBoxToggle={onClickCheckBoxToggle}
+                style={style} />
+        )
+    }, [onClickRemoveBtn, onClickCheckBoxToggle, toDos])
+
+    return (
+        <List className='ToDoList'
+            width={512} // 항목의 넓이
+            height={513} // 항목이 들어갈 list의 전체의 높이
+            rowCount={toDos.length} // 전체 데이터 개수
+            rowHeight={57} // 항목의 높이
+            rowRenderer={rowRenderer} // 행을 만들어줄 함수 
+            list={toDos}
+            style={{ outline: 'none' }}>
+        </List>
+    )
 }
 
 export default ToDoList
--- react_prac4/src/components/ToDoListItem.jsx
+++ react_prac4/src/components/ToDoListItem.jsx
@@ -1,4 +1,4 @@
-import React, {useCallback} from 'react'
+import React, { useCallback } from 'react'
 import './ToDoListItem.scss'
 import {
     MdCheckBoxOutlineBlank,
@@ -7,29 +7,31 @@ import {
 } from 'react-icons/md'
 import cn from 'classnames';
 
-const ToDoListItem = ({toDo, onClickRemoveBtn, onClickCheckBoxToggle}) => {
+const ToDoListItem = ({ toDo, onClickRemoveBtn, onClickCheckBoxToggle, style }) => {
 
     // ToDoList 로 부터 넘어온 데이터 중 text와 checked만 분해
-    const {text, checked, id} = toDo;
-    
+    const { text, checked, id } = toDo;
+
     const onDelete = useCallback((e) => {
         const result = window.confirm(`${text}를 정말로 삭제 하시겠습니까?`);
-        if(result){
+        if (result) {
             onClickRemoveBtn(id);
         }
     }, [onClickRemoveBtn, id, text])
 
-  return (
-    <div className='ToDoListItem'>
-        <div className={cn('checkbox', {checked})} onClick={(e) => {onClickCheckBoxToggle(id)}}>
-            {checked? <MdCheckBox/> : <MdCheckBoxOutlineBlank/>}
-            <div className='text'>{text}</div>
+    return (
+        <div className='ToDoListItem-virtualized'>
+            <div className='ToDoListItem'>
+                <div className={cn('checkbox', { checked })} onClick={(e) => { onClickCheckBoxToggle(id) }}>
+                    {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+                    <div className='text'>{text}</div>
+                </div>
+                <div className='remove' onClick={onDelete}>
+                    <MdRemoveCircleOutline />
+                </div>
+            </div>
         </div>
-        <div className='remove' onClick={onDelete}>
-            <MdRemoveCircleOutline/>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default React.memo(ToDoListItem)
